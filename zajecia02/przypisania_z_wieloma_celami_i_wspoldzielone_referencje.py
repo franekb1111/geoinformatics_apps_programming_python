b0bed5397c6f09b06d04072ae2316976a8246eee
Franek <franekb1111@gmail.com>
2024-03-27 14:52:32 +0100

    not fully done

---
+++ zajecia02/przypisania_z_wieloma_celami_i_wspoldzielone_referencje.py
@@ -0,0 +1,11 @@
+#zad 10
+a = b = [1, 2, 3] 
+b[0] = 'zmieniono'
+
+print(a)
+print(b)
+
+#zmiana b wpłynęła również na a ponieważ przypisanie nastąpiło poprzez referncję
+#lista jest mutowalna
+
+#zad 11
